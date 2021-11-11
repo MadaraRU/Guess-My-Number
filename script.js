7e463365e8ffd01bdf44595967090cd0e6ac2401
MadaraRU <78321649+MadaraRU@users.noreply.github.com>
2021-11-11 17:54:30 +0100

    Guess my number

---
+++ script.js
@@ -0,0 +1,69 @@
+'use strict';
+
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+
+const displayMessage = function(message) {
+    document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess);
+    //when guess i 0
+    if (!guess) {
+        displayMessage('⛔ No number!');
+        //when guess is right
+    } else if (guess === secretNumber) {
+        // document.querySelector('.message').textContent = '🎉 Correct Number!';
+        displayMessage('🎉 Correct Number!');
+        document.querySelector('.number').textContent = secretNumber;
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').style.width = '30rem';
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
+    } else if (guess !== secretNumber) {
+        if (score > 1) {
+            // document.querySelector('.message').textContent =
+            //     guess > secretNumber ? '📈 Too high!' : '📉 Too low!';
+            displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+            score--;
+            document.querySelector('.score').textContent = score;
+        } else {
+            displayMessage('💥 Game Over!');
+            document.querySelector('body').style.backgroundColor = 'red';
+            document.querySelector('.score').textContent = 0;
+        }
+    }
+});
+
+// document.querySelector('.again').addEventListener('click', function() {
+//     window.location.reload();
+// });
+
+// C O D I N G   C H A L L A N G E #1
+document.querySelector('.again').addEventListener('click', function() {
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    score = 20;
+    document.querySelector('.guess').value = '';
+    document.querySelector('.score').textContent = score;
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.number').style.width = '15rem';
+    document.querySelector('.number').textContent = '?';
+    displayMessage('Start guessing...');
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+});
